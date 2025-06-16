a8f66e28a7a4ecc4ce3efb1fffd2193ebb090df8
Anuar234 <104306302+Anuar234@users.noreply.github.com>
2025-06-17 00:39:58 +0500

    first commit

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	server := &http.Server{
+		Addr: ":3000",
+		Handler: http.HandlerFunc(basicHandler), 
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("failed to listen to server", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet{
+		// handle get
+		if r.URL.Path == "/foo" {
+			// handle get foo
+		}
+	}
+
+	if r.Method == http.MethodPost{
+		// handle post
+	}
+	
+	
+	
+	
+	w.Write([]byte("Hello, world!"))
+}
